ab512f3263f08439cef6ba159ad702afd046c77c
Treimart <treier@tlu.ee>
2023-09-27 17:23:25 +0300

    Implement data fetching from URL

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { useState } from "react"
 import "./App.css"
+import Maths from "./components/Maths"
 import Toggle from "./components/Toggle"
 import Show from "./components/Show"
 import MyForm from "./components/MyForm"
+import Fetching from "./components/Fetching"
 
 function App() {
   const [show, setShow] = useState(true)
@@ -12,8 +14,9 @@ function App() {
   return (
     <div className="Main">
       <h1>Märten</h1>
-      <MyForm />
+      <Fetching />
       <Show show={show} />
+      <MyForm />
       <Toggle toggleShow={toggleShow} />
     </div>
   )
---
+++ src/components/PropDrilling.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const PropDrilling = () => {
+  const [weather, setWeather] = useState("☀️")
+
+  return <div>PropDrilling</div>
+}
+
+const Europe = () => {}
+
+const Estonia = () => {}
+
+const Tallinn = () => {}
+
+export default PropDrilling
