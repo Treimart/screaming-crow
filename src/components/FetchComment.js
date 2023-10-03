c511f6b607993f8b45a15dba08e63f13d6b8dcda
Treimart <treier@tlu.ee>
2023-10-03 19:17:34 +0300

    Reimplementation and fix for fetching posts and comments

--- src/components/FetchComment.js
+++ src/components/FetchComment.js
@@ -1,73 +1,50 @@
-import React, { useEffect } from "react"
-import { useState } from "react"
+import React, { useState, useEffect } from "react"
 
 const FetchComment = () => {
-  const [data, setData] = useState([])
-
-  const fetchData = async () => {
-    const postNum1 = Math.floor(Math.random() * 100) + 1
-    const postNum2 = Math.floor(Math.random() * 100) + 1
-
-    const urls = [
-      `https://jsonplaceholder.typicode.com/todos/${postNum1}`,
-      `https://jsonplaceholder.typicode.com/todos/${postNum2}`
-    ]
-
-    const allData = await Promise.all(
-      urls.map(async url => {
-        const response = await fetch(url)
-        const post = await response.json()
-
-        const commentNum1 = Math.floor(Math.random() * 5) + 1
-        const commentNum2 = Math.floor(Math.random() * 5) + 1
-
-        const commentUrls = [
-          `https://jsonplaceholder.typicode.com/comments?postId=${post.id}&id=${
-            (post.id - 1) * 5 + commentNum1
-          }`,
-          `https://jsonplaceholder.typicode.com/comments?postId=${post.id}&id=${
-            (post.id - 1) * 5 + commentNum2
-          }`
-        ]
-
-        const comments = await Promise.all(
-          commentUrls.map(async url => {
-            const response = await fetch(url)
-            return response.json()
-          })
-        )
-
-        return { ...post, comments }
-      })
-    )
-
-    setData(allData)
-    console.log(allData)
-  }
+  const [data, setData] = useState([
+    { post: {}, comment: {} },
+    { post: {}, comment: {} }
+  ])
 
   useEffect(() => {
+    const fetchData = async () => {
+      const commentsResponse = await fetch(
+        "https://jsonplaceholder.typicode.com/comments"
+      )
+      const comments = await commentsResponse.json()
+
+      for (let i = 0; i < 2; i++) {
+        const postId = Math.floor(Math.random() * 100) + 1
+        const postResponse = await fetch(
+          `https://jsonplaceholder.typicode.com/posts/${postId}`
+        )
+        const post = await postResponse.json()
+
+        const relatedComments = comments.filter(
+          comment => comment.postId === post.id
+        )
+        const randomComment =
+          relatedComments[Math.floor(Math.random() * relatedComments.length)]
+
+        data[i] = { post, comment: randomComment }
+      }
+
+      setData([...data])
+    }
+
     fetchData()
   }, [])
 
   return (
     <div>
-      <h2>Featured posts</h2>
-      {data && (
-        <div>
-          {data.map(post => (
-            <div key={post.id}>
-              <h3>
-                {post.id} {post.title}
-              </h3>
-              <b>Comments:</b>
-              {post.comments.map(comment => (
-                <p key={comment.id}>{comment.name}</p>
-              ))}
-              <br />
-            </div>
-          ))}
+      <h2>Posts:</h2>
+      {data.map((item, index) => (
+        <div key={index}>
+          <h3>{item.post.title}</h3>
+          <b>Comments:</b>
+          {item.comment && <p>{item.comment.body}</p>}
         </div>
-      )}
+      ))}
     </div>
   )
 }
