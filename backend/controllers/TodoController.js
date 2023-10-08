6ddea2fb77a05f85a37acb53676e76c6119bf53a
Treimart <treier@tlu.ee>
2023-10-08 14:36:56 +0300

    Add uuid package

---
+++ backend/controllers/TodoController.js
@@ -0,0 +1,10 @@
+let todo = [
+  {
+    id: "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d",
+    title: "Bring milk",
+    priority: 0,
+    created: 1696588906169,
+    updated: null,
+    deleted: false
+  }
+]
